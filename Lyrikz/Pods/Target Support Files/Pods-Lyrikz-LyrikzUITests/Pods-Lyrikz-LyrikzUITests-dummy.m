3fe700bc635d41fd8ce626cc4030eace50e134d2
Mel <melanie.vaubien@hotmail.com>
2021-01-07 09:09:23 +0000

    Add podfile and dependencies

---
+++ Lyrikz/Pods/Target Support Files/Pods-Lyrikz-LyrikzUITests/Pods-Lyrikz-LyrikzUITests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_Lyrikz_LyrikzUITests : NSObject
+@end
+@implementation PodsDummy_Pods_Lyrikz_LyrikzUITests
+@end
---
+++ Lyrikz/Pods/Target Support Files/Pods-Lyrikz/Pods-Lyrikz-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_Lyrikz : NSObject
+@end
+@implementation PodsDummy_Pods_Lyrikz
+@end
